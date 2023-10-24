d5553640b53ac0a1206f7ef44c034f369f269b26
edon99 <ofoura07@gmail.com>
2023-10-24 17:41:21 +0100

    minor adjustments

--- webTourism/webTourism/asgi.py
+++ webTourism/webTourism/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from tourism.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webTourism.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
